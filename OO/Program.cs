2884af914df8dd023dce32ef487998350eb928c9
William Santana <williamandrade1058@gmail.com>
2024-03-02 23:41:22 -0300

    up

---
+++ OO/Program.cs
@@ -0,0 +1,13 @@
+Musica musica1 = new Musica();
+Musica musica2 = new Musica();
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 273;
+musica1.disponivel = true;
+musica1.ExibirFichaTecnica();
+Console.WriteLine();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disponivel = false;
+musica2.ExibirFichaTecnica();
